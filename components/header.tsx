60f7fb6a1e72f21652b6a416ebc5f25fc8fafe42
ebazhanov <evg.bazhanov@yahoo.com>
2025-09-24 14:35:36 +0200

    some fixes

--- components/header.tsx
+++ components/header.tsx
@@ -18,10 +18,10 @@ export function Header() {
             {t("nav.partnerships")}
           </a>
           <a href="#vision" className="hover:text-accent transition-colors">
-            Our Vision
+            {t("vision.title")}
           </a>
           <a href="#value" className="hover:text-accent transition-colors">
-            Why Visma?
+            {t("value.title")}
           </a>
           <a href="#contact" className="hover:text-accent transition-colors">
             {t("nav.contact")}
